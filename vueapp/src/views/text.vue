bfc89106da18cdee411ddfc7cae11f8dd1cdc20a
yuanwengang <y17633833862@163.com>
2020-12-16 14:01:55 +0800

    text.vue

---
+++ vueapp/src/views/text.vue
@@ -0,0 +1,19 @@
+ <template>
+   <div>
+       达到
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+   data () {
+     return {
+     }
+   }
+ }
+ </script>
+ 
+ <style  scoped>
+ 
+ </style>
+ 
