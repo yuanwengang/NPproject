0a03bee94569ec4614fb7681be20a6e48da5b0ca
yuanjiaojiao <18638520107@163.com>
2020-12-16 16:11:11 +0800

    --------------------------

--- vueapp/src/views/Home/BB.vue
+++ vueapp/src/views/Home/BB.vue
@@ -1,5 +1,5 @@
 <template>
-<<<<<<< HEAD
+
     <div>
         ====姣姣啊哈哈哈哈哈哈
     </div>
@@ -14,22 +14,3 @@
 <style scoped>
 
 </style>
-=======
-  <div>
-88888
-  </div>
-</template>
-
-<script>
-export default {
-  data () {
-    return {
-    }
-  }
-}
-</script>
-
-<style  scoped>
-
-</style>
->>>>>>> 658c18262060ad62bf69ea5c5d43f53e2de904cd
